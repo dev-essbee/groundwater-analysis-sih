92d6a3bbe9520f46ff48c39a5911e36dd66c2f49
Shubham Bhandari <dev.sb18@gmail.com>
2020-07-26 08:30:36 +0530

    used pre and post monsoon approach, fixed territorries, added stations used.

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -45,13 +45,15 @@ app.title = 'Analysis GroundWater India'
 #     bucket_name
 #     folder_name
 
-with open(data + r'geojson/gadm36_IND_2_id.json', 'r') as f:
-    districts_geojson = json.load(f)
-df_gw_dis_mont = pd.read_parquet(data + r'gw-district-monthly.parquet.gzip')
+df_gw_pre_post = pd.read_parquet(data + r'comp/gw-block-pre-post.parquet.gzip')
+with open(data + r'geojson/gadm36_IND_0_id.json', 'r') as f:
+    india_geojson = json.load(f)
 with open(data + r'geojson/gadm36_IND_1_id.json', 'r') as f:
     states_geojson = json.load(f)
-df_gw_state_mont = pd.read_parquet(data + r'gw-state-monthly.parquet.gzip')
-
+with open(data + r'geojson/gadm36_IND_2_id.json', 'r') as f:
+    districts_geojson = json.load(f)
+with open(data + r'geojson/gadm36_IND_3_id.json', 'r') as f:
+    blocks_geojson = json.load(f)
 # components viz
 # main map
 
@@ -60,17 +62,28 @@ df_gw_state_mont = pd.read_parquet(data + r'gw-state-monthly.parquet.gzip')
 resolution_main_dropdown = dcc.Dropdown(
     id='resolution-main-map',
     options=[
-        {'label': 'States & UT', 'value': 'states'},
-        {'label': 'Districts', 'value': 'districts'},
-    ], value='states'
+        {'label': 'India', 'value': 'india'},
+        {'label': 'States & UT', 'value': 'state'},
+        {'label': 'Districts', 'value': 'district'},
+        {'label': 'Blocks', 'value': 'block'},
+    ], value='india'
+)
+time_main_dropdown = dcc.Dropdown(
+    id='time-main-map',
+    options=[
+        {'label': 'Pre-Monsoon', 'value': 'pre'},
+        {'label': 'Post-Monsoon', 'value': 'post'},
+    ], value='pre'
 )
 main_map_slider = dcc.Slider(
     id='slider-main-map',
-    # max=list(df_gw_dis_mont.columns)[:-2][-1],
-    max=len(list(df_gw_dis_mont.columns)[:-2]) - 1,
+    max=len(list(df_gw_pre_post.columns)[:-2]) // 3,
     min=0,
     value=0,
-    marks={str(i): str(list(df_gw_dis_mont.columns)[i]) for i in range(len(list(df_gw_dis_mont.columns)[:-2]))},
+    marks={
+        str(i): str(int(list(df_gw_pre_post.columns)[3].split('-')[0]) + i)
+        for i in range(0, (df_gw_pre_post.shape[1] // 3) - 1)
+    },
     step=None
 )
 navbar = dbc.NavbarSimple(
@@ -89,7 +102,14 @@ navbar = dbc.NavbarSimple(
 )
 main_map_card = dbc.Card(
     [
-        dbc.CardHeader(resolution_main_dropdown),
+        dbc.CardHeader(
+            dbc.Row(
+                [
+                    dbc.Col(resolution_main_dropdown),
+                    dbc.Col(time_main_dropdown)
+                ]
+            )
+        ),
         dbc.CardBody(
             [
                 html.H4("Map", className="card-title"),
@@ -148,6 +168,7 @@ app.layout = html.Div(
 def update_stats_callback(hover_data):
     if hover_data:
         val = hover_data['points'][0]
+        # print(hover_data)
         return (
             ([
                 html.H4(val['z'], className='card-title'),
@@ -156,8 +177,11 @@ def update_stats_callback(hover_data):
                 html.H4(val['location'], className='card-title'),
                 html.P('Location', className='card-text'),
             ]), ([
-                html.H4(val['z'], className='card-title'),
-                html.P('Prediction', className='card-text'),
+                # html.H4(val['customdata'].split('-_-')[0] + '/' + val['customdata'].split('-_-')[1],
+                #         className='card-title'),
+                html.H4(val['customdata'],
+                        className='card-title'),
+                html.P('Stations Considered', className='card-text'),
             ])
         )
     else:
@@ -178,22 +202,51 @@ def update_stats_callback(hover_data):
 @app.callback(
     Output(component_id='main-map', component_property='figure'),
     [Input('resolution-main-map', 'value'),
-     Input('slider-main-map', 'value')]
+     Input('slider-main-map', 'value'),
+     Input('time-main-map', 'value')]
 )
 # todo: insert progress bar to update from state to district
-def update_main_map(resolution_level, slider_value):
-    print(slider_value, resolution_level)
-    if resolution_level == 'districts':
-        geojson_file = districts_geojson
-        df_gw = df_gw_dis_mont
+def update_main_map(resolution_level, slider_value, time_value):
+    # print('start', resolution_level, slider_value, time_value)
+    year = str(int(slider_value) + 1994)
+    col_reqd = year + '-' + str(time_value)
+    if resolution_level == 'india':
+        geojson_file = india_geojson
+        df_gw = df_gw_pre_post.agg({col_reqd: ['mean'], year + '-stations': ['sum'], 'total-stations': ['sum']}).round(
+            2)
+        # df_gw.columns = df_gw.columns.droplevel(0)
+        # print(df_gw.loc['mean', col_reqd])
+        z_val = [df_gw.loc['mean', col_reqd]]
+        loc = ['india']
+        used_stations = str(int(df_gw.loc['sum', year + '-stations']))
+        total_stations = str(
+            int(df_gw.loc['sum', 'total-stations']))
+        stations_info = [used_stations + '/' + total_stations]
+        # print(z_val, loc)
     else:
-        geojson_file = states_geojson
-        df_gw = df_gw_state_mont
+        df_gw = df_gw_pre_post.groupby(resolution_level).agg(
+            {col_reqd: ['mean'], year + '-stations': ['sum'], 'total-stations': ['sum']}).round(2)
+        # print(df_gw)
+        used_stations = list(map(int, df_gw.loc[:, (year + '-stations', 'sum')]))
+        total_stations = list(map(int, df_gw.loc[:, ('total-stations', 'sum')]))
+        stations_info = [str(used_stations[i]) + '/' + str(total_stations[i]) for i in range(len(used_stations))]
+        df_gw = df_gw.loc[:,
+                (year + '-' + str(time_value), 'mean')]
+        z_val = df_gw.tolist()
+        loc = df_gw.index.tolist()
+        if resolution_level == 'state':
+            geojson_file = states_geojson
+        elif resolution_level == 'district':
+            geojson_file = districts_geojson
+        else:
+            geojson_file = blocks_geojson
+
     fig_map = go.Figure(
         go.Choroplethmapbox(
             geojson=geojson_file,
-            locations=df_gw.index.astype(str),
-            z=df_gw['2020-06'].astype(float),
+            locations=loc,
+            z=z_val,
+            customdata=stations_info,
             colorscale="Viridis",
             zmin=0,
             zmax=12,
